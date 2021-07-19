fa1dcdacaae74ce63ba10f6d5438d09033a10270
BarConT <diego_9deabril@hotmail.com>
2021-07-19 00:13:58 -0300

    Pruebas unitarias

--- encuestas/tests.py
+++ encuestas/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class MyIntegrationTest(TestCase):
+
+    def setUp(self) -> None:
+        '''
+            Inicializa el set de pruebas.
+        '''
+        return super().setUp()
+
+    def testHome(self):
+        response = self.client.get("/")
+        self.assertContains(response , "Team 5, Codo a Codo")
